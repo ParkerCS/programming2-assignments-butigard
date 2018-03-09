627c9945986fbef45e8b90551c0480599104f37f
butigard <butigard@fwparker.org>
2018-03-09 09:05:21 -0600

    new

--- Spell Check Lab.py
+++ Spell Check Lab.py
@@ -5,10 +5,31 @@ def split_line(line):
 
 file = open('search_files/dictionary.txt')
 dic = []
-largest = "YEET"
-large_list = []
+
 
 for line in file:
     words = split_line(line)
     for word in words:
         dic.append(word)
+print(dic)
+
+Alice = open('search_files/AliceInWonderLand200.txt')
+AliceWords = []
+
+for line in Alice:
+    word = split_line(line)
+    for words in word:
+        AliceWords.append(words)
+print(AliceWords)
+
+
+for i in range(len(AliceWords)):
+    word = AliceWords[i]
+    u = 0
+    while u < len(dic) and word.upper() != dic[u]:
+        u += 1
+
+    if u < len(dic):
+        print("Found", word, "at position", u)
+    else:
+        print("Word", word, "not found in dictionary")
